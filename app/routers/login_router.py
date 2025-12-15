909fac21ca61f03f012fddce9a600db97a449ab5
vladmikhbond <vladmikhbond@gmail.com>
2025-12-15 14:54:03 +0000

    .

--- app/routers/login_router.py
+++ app/routers/login_router.py
@@ -1,13 +1,16 @@
 import os
+import bcrypt
 
 from fastapi.security import APIKeyCookie
 import httpx
 import jwt
 
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi import APIRouter, HTTPException, Request, Form, Response, Security
+from fastapi import APIRouter, Depends, HTTPException, Request, Form, Response, Security
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+from app.dal import get_users_db
 
 from ..models.pss_models import User
 
@@ -21,6 +24,51 @@ templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter()
 
+# =============================== aux ==================================
+
+# описуємо джерело токена (cookie)
+cookie_scheme = APIKeyCookie(name="access_token")
+
+def get_current_user(token: str = Security(cookie_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    else:
+        return User(username=payload.get("sub"), role=payload.get("role"))
+
+
+
+
+# ----------------------- password
+
+@router.get("/pass")
+async def get_pass(
+    request: Request, 
+    user: User = Depends(get_current_user) 
+):
+    return templates.TemplateResponse("login/pass.html", {"request": request})
+
+@router.post("/pass")
+async def post_pass (
+    request: Request,
+    password: str = Form(...),
+    db: Session = Depends(get_users_db),
+    current_user: User = Depends(get_current_user) 
+):
+    user = db.get(User, current_user.username);
+    if not user:
+        raise HTTPException(400)
+    user.hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    try:
+        db.commit()
+    except:
+        error = "Не вдалося змінити пароль"
+        return templates.TemplateResponse("login/pass.html", {"request": request, "error": error}) 
+     
+    return templates.TemplateResponse("login/login.html", {"request": request})
 # ----------------------- login
 
 @router.get("/")
@@ -67,31 +115,14 @@ async def login(
     )
     return redirect    
 
+# ----------------------------------- logout
+
 @router.get("/logout")
-async def logout(response: Response):
+async def logout(response: Response, request: Request):
     response.delete_cookie(
         key="access_token"
     )
+    return templates.TemplateResponse("/login/login.html", {"request": request} )
+
     return {"message": "Session ended"}   
 
-# =================================================================
-
-# описуємо джерело токена (cookie)
-cookie_scheme = APIKeyCookie(name="access_token")
-
-def get_current_user(token: str = Security(cookie_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Token expired")
-    except jwt.InvalidTokenError:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    else:
-        return User(username=payload.get("sub"), role=payload.get("role"))
-
-
-# def get_current_tutor(token: str = Security(cookie_scheme)):
-#     user = get_current_user(token)
-#     if user.role != "tutor":
-#         raise HTTPException(status_code=403, detail="Permission denied: tutors only.")
-#     return user
